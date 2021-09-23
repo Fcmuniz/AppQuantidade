3209e62a9662f30836e64f34cc93d6213d70c2a3
Felipe Muniz <39996206+Fcmuniz@users.noreply.github.com>
2021-09-23 14:57:34 -0400

    AddCode

--- AppQuantidade/AppQuantidade/MainPage.xaml.cs
+++ AppQuantidade/AppQuantidade/MainPage.xaml.cs
@@ -8,11 +8,30 @@ using Xamarin.Forms;
 
 namespace AppQuantidade
 {
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        int quantidade = 1;
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void Aumentar(object sender, EventArgs e)
+        {
+            quantidade++;
+            LblQuantidade.Text = quantidade.ToString();
+        }
+
+        private void Diminuir(object sender, EventArgs e)
+        {
+            if (quantidade != 0)
+            {
+                quantidade--;
+                LblQuantidade.Text = quantidade.ToString();
+            }
         }
     }
 }
